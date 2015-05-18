74c368d506d7792cbf6276115e99cdba02cd1b95
robbie <robbie@weebly.com>
2015-05-18 14:59:39 -0700

    updating config

--- config/config.ini.php
+++ config/config.ini.php
@@ -5,7 +5,7 @@ host = us-cdbr-iron-east-02.cleardb.net
 username = b1b5ef915e1b6f
 password = 5e89decc
 dbname = heroku_51c3173c53f5f4b
-tables_prefix = piwik_
+tables_prefix = 
 port = 3306
 adapter = PDO\MYSQL
 type = InnoDB
@@ -14,4 +14,3 @@ schema = Mysql
 [Mbstring]
 mbstring.func_overload = 0
 
-session_save_handler = dbtable
