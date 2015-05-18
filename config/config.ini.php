e234379c4a5ccb5da20a4de59294e4817ae6cc3f
robbie <robbie@weebly.com>
2015-05-18 14:54:17 -0700

    updating config, still not right

--- config/config.ini.php
+++ config/config.ini.php
@@ -1,6 +1,5 @@
 <?php exit; ?>
 
-mysql://b1b5ef915e1b6f:5e89decc@us-cdbr-iron-east-02.cleardb.net/heroku_51c3173c53f5f4b?reconnect=true
 [database]
 host = us-cdbr-iron-east-02.cleardb.net
 username = b1b5ef915e1b6f
