76b1f74458bc4f843f7661cbce9370abf9f6ef6d
robbie <robbie@weebly.com>
2015-05-18 14:33:36 -0700

    updating config.ini.php

--- config/config.ini.php
+++ config/config.ini.php
@@ -1,20 +1,19 @@
+<?php
+$db = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+echo '[database]';
+echo 'host = ' . $db["host"] . "\n";
+echo 'username = ' . $db["username"] . "\n";
+echo 'password = ' . $db["password"] . "\n";
+echo 'port = ' . $db["port"] . "\n";
+echo 'dbname = ' . substr($db["path"], 1) . "\n";
+echo 'tables_prefix = piwik_' . "\n";
+echo 'adapter = PDO\MYSQL' . "\n";
+echo 'type = InnoDB' . "\n";
+echo 'schema = Mysql' . "\n";
+?>
+
 [Mbstring]
 mbstring.func_overload = 0
-
-[database]
-host = %database_host%
-username = %database_user%
-password = %database_password%
-dbname = %database_name%
-tables_prefix = piwik_
-port = %database_port%
-adapter = PDO\MYSQL
-type = InnoDB
-schema = Mysql
-
-; if charset is set to utf8, Piwik will ensure that it is storing its data using UTF8 charset.
-; it will add a sql query SET at each page view.
-; Piwik should work correctly without this setting.
-;charset = utf8
 
 session_save_handler = dbtable
