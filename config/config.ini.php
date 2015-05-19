bedddbc608f4885b3b03ee88aed783f3f7067e87
robbie <robbie@weebly.com>
2015-05-18 17:42:46 -0700

    adding config.ini.php

---
+++ config/config.ini.php
@@ -0,0 +1,22 @@
+[General] 
+trusted_hosts[] = "docs.weebly.net"
+
+[database]
+host = "us-cdbr-iron-east-02.cleardb.net"
+username = "b1b5ef915e1b6f"
+password = "5e89decc"
+dbname = "heroku_51c3173c53f5f4b"
+tables_prefix = "piwik_"
+port = 3306
+adapter = PDO\MYSQL
+type = InnoDB
+schema = Mysql
+
+[superuser]
+login = "admin"
+password = "beerpong"
+email = "robbie@weebly.com"
+
+[Plugins]
+Plugins[] = VisitorGenerator
+
