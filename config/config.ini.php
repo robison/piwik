7d9d96ff38b1aff5bc9e5db5bef09880da274b86
robbie <robbie@weebly.com>
2015-05-19 13:07:57 -0700

    updating config.ini.php

--- config/config.ini.php
+++ config/config.ini.php
@@ -1,16 +1,17 @@
 <?php
-    $db = parse_url(getenv('CLEARDB_DATABASE_URL'));
+    define('db', parse_url(getenv('CLEARDB_DATABASE_URL')));
 
     define('DBHOST', $db['host']);
     define('DBUSER', $db['user']);
     define('DBPASS', $db['pass']);
     define('DBNAME', substr($db["path"], 1));
 ?>
+
 [database]
-host = $DBHOST
-username = $DBUSER
-password = $DBPASS
-dbname = $DBNAME
+host = DBHOST
+username = DBUSER
+password = DBPASS
+dbname = DBNAME
 port = 3306
 adapter = PDO\MYSQL
 type = InnoDB
